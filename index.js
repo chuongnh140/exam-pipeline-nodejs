e38c326fbd696ae5e31fb037e88afb7493f165d3
chuongnh140 <chuongnh140@gmail.com>
2021-01-07 15:56:16 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-const app_env = process.env.APP_ENV || "unknown";
+const app_env = process.env.APP_HOST || "unknown";
 const app_version = process.env.APP_VERSION || "unknown";
 
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  
-const msg = `Hello Word! Application version ${app_version} - Environment ${app_env}\n`
+
+const msg = `Hello Word! Application version ${app_version} - Environment ${app_host}\n`
   res.end(msg);
 });
 
